004c964e16f3db58b16e8916b417a46bca7f87e5
dikiyrot <dikiyrot@gmail.com>
2021-09-18 16:21:14 +0300

    component Viewers

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import ImgSlider from './ImgSlider';
+import Viewers from './Viewers';
 
 const Container = styled.main`
    min-height: calc(100vh - 70px);
@@ -25,6 +26,7 @@ const Home = () => {
    return (
       <Container>
          <ImgSlider />
+			<Viewers />
       </Container>
    );
 };
