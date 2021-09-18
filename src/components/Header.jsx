40ec865a54eb3374e775a5372c6deb5bf35cf039
dikiyrot <dikiyrot@gmail.com>
2021-09-18 13:26:48 +0300

    component header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,95 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Nav = styled.div`
+const Nav = styled.nav`
    height: 70px;
    background-color: #090b13;
+   display: flex;
+   align-items: center;
+   padding: 0 36px;
 `;
 const Logo = styled.img`
    width: 80px;
 `;
 const NavMenu = styled.div`
-	
+   display: flex;
+   flex: 1 1 auto;
+   margin-left: 25px;
+   align-items: center;
+   a {
+      display: flex;
+      align-items: center;
+      padding: 0 12px;
+      cursor: pointer;
+      img {
+         height: 20px;
+      }
+      span {
+         font-size: 13px;
+         letter-spacing: 1.42px;
+         position: relative;
+
+         &::after {
+            content: '';
+            height: 1px;
+            background-color: #fff;
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: -6px;
+            opacity: 0;
+				transition: all 0.25s ease 0s;
+            transform: scaleX(0.5);
+         }
+      }
+      &:hover {
+         span {
+            &::after {
+               opacity: 1;
+               transform: scaleX(1);
+            }
+         }
+      }
+   }
 `;
-// const Nav = styled.div``
+const UserImage = styled.img`
+   width: 48px;
+   height: 48px;
+   border-radius: 50%;
+   cursor: pointer;
+`;
 
 const Header = () => {
    return (
       <Nav>
-         <Logo src="" />
-			<NavMenu>
-			
-			</NavMenu>
+         <Logo src="/images/logo.svg" />
+         <NavMenu>
+            <a>
+               <img src="/images/home-icon.svg" />
+               <span>HOME</span>
+            </a>
+            <a>
+               <img src="/images/search-icon.svg" />
+               <span>SEARCH</span>
+            </a>
+            <a>
+               <img src="/images/watchlist-icon.svg" />
+               <span>WATCHLIST</span>
+            </a>
+            <a>
+               <img src="/images/watchlist-icon.svg" />
+               <span>WATCHLIST</span>
+            </a>
+            <a>
+               <img src="/images/movie-icon.svg" />
+               <span>MOVIE</span>
+            </a>
+            <a>
+               <img src="/images/series-icon.svg" />
+               <span>SERIES</span>
+            </a>
+         </NavMenu>
+         <UserImage src="https://w7.pngwing.com/pngs/869/276/png-transparent-smiley-emoticon-smiley-miscellaneous-desktop-wallpaper-world-smile-day-thumbnail.png" />
       </Nav>
    );
 };
